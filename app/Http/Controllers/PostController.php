4a330b0f49c6ca222e8909f404299711b7c74fb1
Rouurouu <rayendjebbi2022@gmail.com>
2024-04-06 17:06:30 +0100

    query relationship

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
     */
     public function index()
     {
-     $posts = Post::all();
+     $posts = Post::with('category')->get();
+     //dd($posts);
      return view('posts.index',compact('posts'));
     }
 
