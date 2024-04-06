f95edc6604fc01a4b80f2a24613a7205b369167e
Rouurouu <rayendjebbi2022@gmail.com>
2024-04-06 17:06:46 +0100

    display posts list

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,40 @@
-<?php dd($posts);?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gl2023</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script> 
+</head>
+<body>
+    <div class="container py-4">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Body</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($posts as $post)
+                <tr>
+                    <th scope="row">{{ $post->id }}</th>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ substr($post->body, 0, 50).'...' }}</td>
+                    <td>{{$post->category->name}}</td>
+                    <td>
+                        <button class="btn btn-outline-info">Show</button>
+                        <button class="btn btn-outline-warning">Edit</button>
+                        <button class="btn btn-outline-danger">Delete</button>   
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>   
+    </div>
+</body>
+</html>
